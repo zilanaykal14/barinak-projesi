9c67a278af4800154aae806086177bbd2da383d5
zilanaykal14 <zilanaykal3@gmail.com>
2025-12-03 12:07:06 +0300

    postgres destegi eklendi

--- shelter-backend/src/app.module.ts
+++ shelter-backend/src/app.module.ts
@@ -1,39 +1,48 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ServeStaticModule } from '@nestjs/serve-static'; // YENİ
-import { join } from 'path'; // YENİ
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { IrkModule } from './irk/irk.module';
 import { HayvanModule } from './hayvan/hayvan.module';
 import { AsiModule } from './asi/asi.module';
-import { BildirimModule } from './bildirim/bildirim.module';
 import { CipModule } from './cip/cip.module';
+import { BildirimModule } from './bildirim/bildirim.module';
 
 @Module({
   imports: [
-    // Resim klasörünü internete aç (Public yap)
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'),
+      rootPath: join(process.cwd(), 'uploads'),
       serveRoot: '/uploads',
     }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'shelter_db',
+      // EĞER RENDER'DAYSAK POSTGRES, YOKSA MYSQL KULLAN
+      type: process.env.DATABASE_URL ? 'postgres' : 'mysql',
+      
+      // RENDER AYARLARI (Otomatik alır)
+      url: process.env.DATABASE_URL,
+      
+      // LOCAL (SENİN BİLGİSAYARIN) AYARLARI
+      host: process.env.DATABASE_URL ? undefined : '127.0.0.1',
+      port: process.env.DATABASE_URL ? undefined : 3306,
+      username: process.env.DATABASE_URL ? undefined : 'root',
+      password: process.env.DATABASE_URL ? undefined : '',
+      database: process.env.DATABASE_URL ? undefined : 'shelter_db',
+      
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: true, // Tabloları otomatik oluşturur
+      
+      // SSL Ayarı (Render için zorunlu)
+      ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
     }),
     UsersModule,
     IrkModule,
     HayvanModule,
     AsiModule,
-    BildirimModule,
     CipModule,
+    BildirimModule,
   ],
   controllers: [AppController],
   providers: [AppService],
