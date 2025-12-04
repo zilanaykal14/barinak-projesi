e1cc3d1248598289f84cb4e629189ccd5f8427f6
zilanaykal14 <zilanaykal3@gmail.com>
2025-12-04 18:30:03 +0300

    uploads yolu duzeltildi

--- shelter-backend/dist/main.js
+++ shelter-backend/dist/main.js
@@ -2,9 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const path = require("path");
+// Express modülünü ekliyoruz
+const express = require("express");
+
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    
     app.enableCors();
+
+    // --- DÜZELTİLEN KISIM BURASI ---
+    // __dirname = dist klasörü demektir.
+    // '..' = Bir üst klasöre çık demektir (Ana dizine).
+    // 'uploads' = Oradaki uploads klasörünü bul demektir.
+    app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+    // -------------------------------
+
     await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
