f1a148d136f6003d93cafa2d93c82748795b4c56
zilanaykal14 <zilanaykal3@gmail.com>
2025-12-04 18:34:49 +0300

    src main ts duzeltildi

--- shelter-backend/src/main.ts
+++ shelter-backend/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as express from 'express'; 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // <--- BU SATIRI EKLE (Frontend bağlanabilsin diye)
+  
+  app.enableCors();
+
+  // --- RESİMLERİ DIŞARIYA AÇAN KOD ---
+  // src klasöründen bir üst klasöre çıkıp uploads'ı buluyoruz
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+  // -----------------------------------
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
