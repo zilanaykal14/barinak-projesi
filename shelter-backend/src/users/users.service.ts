948cd8fc2b8ac1fbe3ec045eec271cf8c901d5e8
zilanaykal14 <zilanaykal3@gmail.com>
2026-01-08 22:58:21 +0300

    Bcryptjs gecisi yapildi

--- shelter-backend/src/users/users.service.ts
+++ shelter-backend/src/users/users.service.ts
@@ -4,7 +4,7 @@ import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
-import * as bcrypt from 'bcrypt'; 
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
